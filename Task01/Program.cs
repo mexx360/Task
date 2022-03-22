588ce552a16c5ac868def39aac307cc0bb3ec62c
Petr Makeev <essential@inbox.ru>
2022-03-22 22:30:09 +0300

    New folder Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число => ");                                     // Вывод приглашения для ввода      
+string strValue = Console.ReadLine();                                       // Ввод строки    
+int value = int.Parse(strValue);                                            // Преобразуем строку в целое число
+
+int result = value * value;                                                 // Вычесление квадрата
+Console.WriteLine ($"Квадрат числа {value} равен: {result}");               // Вывод резуьтат
