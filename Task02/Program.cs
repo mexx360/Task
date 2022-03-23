9a5c360e72d622f528e46b1ba18ede43ede5fb9d
Petr Makeev <essential@inbox.ru>
2022-03-23 22:09:31 +0300

    New folder Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ");
+string inputNunber = Console.ReadLine ();
+int number = int.Parse(inputNunber);
+
+if (number == 1) Console.WriteLine("Понедельник"); else
+if (number == 2) Console.WriteLine("Вторник"); else
+if (number == 3) Console.WriteLine("Среда"); else 
+if (number == 4) Console.WriteLine("Четверг"); else
+if (number == 5) Console.WriteLine("Пятница"); else 
+if (number == 6) Console.WriteLine("Суббота"); else 
+if (number == 6) Console.WriteLine("Воскресенье"); else
+{
+    Console.WriteLine("В неделе 7 дней");
+}
