651a4c8709fa5370da9cd931ed9b4a5fb7cd61cc
Petr Makeev <essential@inbox.ru>
2022-03-24 20:46:50 +0300

    New folder Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа проверяет, является ли первое число квадратом второго.
+Console.Write("Введите число А => ");
+string inputA = Console.ReadLine();
+int numberA = int.Parse(inputA);
+
+Console.Write("Введите число В => ");
+string inputВ = Console.ReadLine();
+int numberB = int.Parse(inputВ);
+
+int value = numberA * numberA;
+if (numberB == value) Console.WriteLine("Да");
+else
+{
+    Console.WriteLine("Нет");
+}
