8e7d9f9e978ad464d8b7a44a8e76d88e1874b2bc
Petr Makeev <essential@inbox.ru>
2022-03-24 21:25:41 +0300

    new folder task04

---
+++ Task04/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа вычисления функции: x=f(a).
+Console.Write("Введите число А => ");
+string inputNumber = Console.ReadLine();
+int numberA = int.Parse(inputNumber);
+
+int x = numberA + 9;
+Console.WriteLine($"Функция x = f(a): {x}");
