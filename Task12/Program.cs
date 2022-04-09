87497c2d719121c06dba498490e2a874946e3fd2
Petr Makeev <essential@inbox.ru>
2022-04-09 14:08:08 +0300

    New folder Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,18 @@
+﻿// Метод перевода из отрицательнго значения числа "-n"  в  "n" значение. 
+
+
+int promt(string strValue)
+{
+    Console.WriteLine(strValue);
+    string Value = Console.ReadLine ();
+    int a = int.Parse(Value);
+    return a;
+}
+
+int M(int a)
+{
+    if (a < 0) a = -a;
+    return a;
+}
+int A = promt("Введите число: ");
+Console.WriteLine(M(A));
