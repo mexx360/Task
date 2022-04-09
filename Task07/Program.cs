758f5a64e82b57b955c91e93042c69004cb52f96
Petr Makeev <essential@inbox.ru>
2022-04-09 10:30:26 +0300

    New folder Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,17 @@
+﻿//      Рандомный метод "2 вариант" 
+ 
+
+int value = RandomInt(1, 10);   
+while (value !=9)                  
+{
+Console.WriteLine($"Случайное значение = {value}");     
+value = RandomInt(1, 10);    
+}
+Console.WriteLine($"Случайное значение = {value}"); 
+
+int RandomInt(int min, int max)
+{
+Random rnd = new Random();
+int value = rnd.Next(min, max);   
+return value;
+}
