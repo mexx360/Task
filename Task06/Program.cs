07668834d77863a196a097c2dc23255e744dd7f4
Petr Makeev <essential@inbox.ru>
2022-04-09 10:08:12 +0300

    New folder Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,11 @@
+﻿//      Рандомный метод "1 вариант" 
+
+
+Random rnd = new Random();
+
+int value = rnd.Next(1, 10);   
+while (value !=9)                  
+{
+Console.WriteLine($"Случайное значение = {value}");     
+value = rnd.Next(1, 10);    
+}
