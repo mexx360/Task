240eb1a4f9d120c7006852dad45b16380f555ba3
Petr Makeev <essential@inbox.ru>
2022-04-12 20:28:46 +0300

    New folder Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и выводит числа без второй цифры.
+
+Random rnd = new Random ();
+int value = rnd.Next (100, 1000);
+int number = value / 100 * 10 + (value % 10);
+Console.WriteLine($"Вывод случайного трехзначного числа {value}");
+Console.WriteLine($"Вывод числа без второй цифры {number}");
