e3bd35830be40a5c4d4f26b2f5b8b2ecd7a217b1
Petr Makeev <essential@inbox.ru>
2022-04-12 21:08:16 +0300

    New folder Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая получает число и выводит третью и последнюю цифру числа.
+
+Console.Write($"Введите число: ");
+string inputN = Console.ReadLine();
+int N = int.Parse (inputN);
+if (N > 100)
+{
+    Console.Write ("Третья цифра: " + N % 10);
+}
+else
+{
+    Console.Write("Нет последней цифры");
+}
+Console.WriteLine ();
+if (N > 100)
+{
+ Console.WriteLine("Последняя цифра: " + N / 10 / 10 % 10);   
+}
