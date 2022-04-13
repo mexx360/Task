ab3d0b31ee6ab4478332f083de3baca8b94a6622
Petr Makeev <essential@inbox.ru>
2022-04-13 20:52:57 +0300

    New folder Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая проверяет соответствие числа дню недели (7 дней).
+
+Console.Write("Введите число: ");
+string inputN = Console.ReadLine();
+int N = int.Parse (inputN);
+if (N == 6 || N == 7)
+Console.WriteLine ($"Число {N} - выходной ");
+else Console.WriteLine ($"Число {N} - не выходной ");
