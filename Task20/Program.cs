38664b27f74a2d972e812d40f4c7453c6b1f6c9a
Petr Makeev <essential@inbox.ru>
2022-04-21 22:26:53 +0300

    New folder Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int Prompt (string message)
+{
+    Console.Write (message);
+    string strValue = Console.ReadLine ();
+    int Value = int.Parse (strValue);
+    return Value;
+}
+    int N = Prompt ("Введите число: ");
+    int A = 1;
+    Console.Write (A+ " ");
+    while (A < N)
+{
+    A++;
+    Console.Write (A*A+ " ");
+}
